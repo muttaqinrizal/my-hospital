778f28b10f1e3c3d713b720ea870b1c5a3c0131b
muttaqinrizal <rizalm57@gmail.com>
2019-07-18 12:55:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      <div style={{fontFamily: 'Arial'}}>
+      <h1 style={styles.items}>hai</h1>
+      <h2 className="color-font">Hello</h2>
+      <Counter/>
+      </div>
+    )
+  }
 }
 
+
+class Counter extends React.Component{
+  state = {
+    angka : 0
+  }
+
+  tambah = ()=>{
+    this.setState({
+      angka : this.state.angka + 1
+    });
+  }
+
+  kurang = ()=>{
+    this.setState({
+      angka : this.state.angka - 1
+    })
+  }
+  render(){
+    return(
+      <div>
+        <h1>{this.state.angka}</h1>
+        <button onClick={this.tambah}>+</button>
+        <button onClick={this.kurang}>-</button>
+      </div>
+    )
+  }
+}
+
+const styles = {
+  items : {
+    color: '#4287f5',
+    fontSize: '30px'
+  }
+}
 export default App;
